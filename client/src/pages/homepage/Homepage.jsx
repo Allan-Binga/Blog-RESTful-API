9b1ea8129a60c18b17dde9d3bb8a752da8bcf682
Allan-Binga <allanbinga73@gmail.com>
2024-11-27 18:59:58 -0400

    Frontend integration pt.3

--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -3,15 +3,26 @@ import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./homepage.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Homepage() {
-  const location = useLocation();
-  console.log(location);
+  // const location = useLocation();
+  // console.log(location);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts");
+      setPosts(res.data)
+    };
+    fetchPosts()
+  }, []);
   return (
     <>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={posts}/>
         <Sidebar />
       </div>
     </>
