55376231efabcfc5f58ba01dfeb97c9c09474b8a
Allan-Binga <allanbinga73@gmail.com>
2025-03-08 03:11:22 -0400

    Server file(index.js) improvements.

--- backend/index.js
+++ backend/index.js
@@ -2,27 +2,37 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/auth.js");
 const userRoute = require("./routes/users.js");
 const postRoute = require("./routes/posts.js");
 const categoryRoute = require("./routes/categories.js");
 
-//testing
 dotenv.config();
 
+//Middleware
 app.use(express.json());
+app.use(cookieParser());
 
 main().catch((err) => console.log(err));
 
+//Database connection function
 async function main() {
-  await mongoose.connect(process.env.MONGO_URL);
-  console.log("MongoDB connected successfully.");
+  try {
+    // Load the variable and attempt a login.
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB connected successfully.");
+  } catch (err) {
+    // Log any connection errors
+    console.log("MongoDB connection error:", err);
+  }
 }
 
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/users", userRoute);
-app.use("/api/v1/posts", postRoute);
-app.use("/api/v1/categories", categoryRoute);
+//Routes - Auth, Users, Posts, Categories
+app.use("/byteblog/v1/auth", authRoute);
+app.use("/byteblog/v1/users", userRoute);
+app.use("/byteblog/v1/posts", postRoute);
+app.use("/byteblog/v1/categories", categoryRoute);
 
 app.listen("5000", () => {
   console.log("Backend up and running.");
